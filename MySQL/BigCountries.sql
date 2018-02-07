f2efd0136e3ff8758390f5c02d6a2cda702dd721
AShiou <aa8693@gmail.com>
2018-02-07 18:58:43 +0800

    595-Big Countries

---
+++ MySQL/BigCountries.sql
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  AShiou
+ * 595.Big Countries    
+ */
+select name,population,area from World where area>3000000 or population>25000000;
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-#leetcode
-760 Find Angram Mappings
-771-Jewels and Stones
+<b>[Easy][JAVA]760 Find Angram Mappings</b>
+<b>[Easy][JAVA]771-Jewels and Stones</b>
+<b>[Easy][MySQL]595-Big Countries</b>
